c252534e37cdc8b59a74a4d135de12e779a48197
Dawid7040 <lol.kakareko@gmail.com>
2023-09-28 10:32:18 +0200

    28.09 Task 3

--- LessonsWWW/admin.py
+++ LessonsWWW/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from LessonsWWW.models import Author, Category, Book
+
 # Register your models here.
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "year_of_birth"]
+    list_filter = ["year_of_birth", "last_name"]
+    search_fields = ["first_name", "last_name"]
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ["category_name"]
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "category", "description"]
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Book, BookAdmin)
--- LessonsWWW/models.py
+++ LessonsWWW/models.py
@@ -6,6 +6,31 @@ class Author(models.Model):
     last_name = models.CharField(max_length=50)
     year_of_birth = models.IntegerField(null=True)
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50)
+    def __str__(self):
+        return f'{self.category_name}'
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        'Author',
+        on_delete=models.CASCADE,
+    )
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.SET_NULL,
+        null=True,
+        default=None
+    )
+    description = models.CharField(max_length=213525, default='', blank=True)
+
+    def __str__(self):
+        return f'{self.title}, {self.author}, {self.category}, {self.description}'
